6e0e881e54ba3ab25e48cc1a30fd23c84346d348
Tom Nchinda <tom@passionatepeople.io>
2022-10-13 16:05:35 +0200

    feat: new home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,68 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+  import { RouterLink } from 'vue-router';
+  import { useShowsStore } from '../stores/shows';
+  import { useRoute } from 'vue-router';
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<script lang="ts">
+  export default {
+    data() {
+      const store = useShowsStore();
+      return {
+        store,
+        bgImage: ''
+      };
+    },
+    methods: {
+      initialise() {
+        this.store.fetchShows().then(() => {
+          this.bgImage = `linear-gradient(45deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0.2) 100%), url(/svg/ABN-AMRO_Logo_new_colors.svg), linear-gradient(45deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0.5) 100%), url(${this.store.shows[Math.floor(Math.random() * this.store.shows.length)].image.original})`;
+        })
+      },
+    },
+    beforeMount(){
+      this.initialise();
+    },
+  }
+</script>
+
+  <template>
+    <main class="main-content">
+      <div class="above-logo">
+        <h1>Welcome to ab<span class="color__primary">N</span><span class="color__secondary">e</span>tflix!</h1>
+        <p>We are the ultimate guide to your favourite TV shows</p>
+        <p>Log in to get your personalised information on every show that ever was and ever will be</p>
+      </div>
+    </main>
+  </template>
+  
+  <style lang="scss" scoped>
+    .main-content {
+      font-family: Roboto, Arial, Helvetica, sans-serif;
+      background: no-repeat center;
+      background-size: contain, 30%, contain, cover;
+      background-image: v-bind(bgImage);
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      min-width: 100vw;
+      min-height: 100vh;
+      position: relative;
+      top: 0;
+      left: 0;
+
+      h1 {
+        text-align: center;
+        span {
+          font-weight: bold;
+          text-shadow: 0px 0px 2px rgba(0,0,0,0.5)
+        }
+      }
+    }
+
+    .above-logo {
+      transform: translateY(-100%);
+    }
+  </style>
+  
