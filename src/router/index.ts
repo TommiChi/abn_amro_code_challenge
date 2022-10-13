e8e9e2240a30857335a7232746ac185766ad5cf3
Tom Nchinda <tom@passionatepeople.io>
2022-10-13 15:25:03 +0200

    feat: new router and business logic for it

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory, useRoute } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import { isLoggedIn } from '../services/user';
+import { useShowsStore } from '../stores/shows';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,17 +9,37 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/showsguide',
+      name: 'showsguide',
+      component: () => {
+        const store = useShowsStore();
+        store.initUserHome();
+        return import('../views/ShowsHomeView.vue');
+      },
+    },
+    {
+      path: '/shows/:id',
+      name: 'showdetails',
+      component: () => {
+        const store = useShowsStore();
+        const episodeId = store.detailsPath.split('/').pop() || '';
+        store.selectEpisode(episodeId);
+        return import('../views/ShowDetailsView.vue');
+      },
+      props: true,
+    },
   ]
 })
 
-export default router
+router.beforeEach(route => {
+  const store = useShowsStore();
+  store.setDetailsPath(route.fullPath);
+  if (route.fullPath !== '/' && !isLoggedIn()) {
+    router.replace('/');
+  }
+});
+
+export default router;
