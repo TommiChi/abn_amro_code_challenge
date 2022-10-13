6e910e67789fa94e81699894216068f2cc22ad96
Tom Nchinda <tom@passionatepeople.io>
2022-10-13 16:10:03 +0200

    feat: navigation component

--- src/components/Navigation/Navigation.vue
+++ src/components/Navigation/Navigation.vue
@@ -1,20 +1,65 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router';
-import User from './User.vue';
+  import { RouterLink } from 'vue-router';
+  import User from './User.vue';
+  import { useUserStore } from '../../stores/user';
+</script>
+
+<script lang="ts">
+  export default {
+    data() {
+      const store = useUserStore();
+      return {
+        store
+      };
+    },
+    computed: {
+      hasLoggedIn () {
+        return this.store.isLoggedIn ? '' : ' root-home';
+      }
+    }
+  }
 </script>
 
 <template>
-  <header class="header">
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/tv-guide">TV Guide</RouterLink>
-      <RouterLink to="/my-shows">My Shows</RouterLink>
-      <RouterLink to="/categories">Catgegories</RouterLink>
-    </nav>
+  <header :class="`header${hasLoggedIn}`">
+    <span class="greeting" v-if="store.isLoggedIn">
+      Welcome back, {{ store.name }}!
+    </span>
 
-    <User />
+    <User class="user-controls" />
   </header>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+  .header {
+    position: fixed;
+    height: 50px;
+    width: 100vw;
+    box-shadow: 0 -5px 10px 1px;
+    top: 0;
+    left: 0;
+    z-index: 10000;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    background-color: rgba(243, 192, 0, 0.1);
+
+    &.root-home {
+      box-shadow: none;
+    }
+  }
+
+  .user-controls {
+    flex-grow: 1;
+  }
+
+  .greeting {
+    color: #009286;
+    flex-grow: 1;
+    text-align: center;
+    transform: translate(50%);
+    font-size: 20px;
+    // color: #f3c000;
+  }
 </style>
